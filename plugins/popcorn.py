5a70790f6f83edceab1d57999e07e762d0a5aea0
RZ Code Studio <73451384+To-Code-Or-Not-To-Code@users.noreply.github.com>
2022-12-29 20:20:18 -0800

    Update code

--- plugins/popcorn.py
+++ plugins/popcorn.py
@@ -5,6 +5,7 @@ import random
 
 hint = 0
 
+
 class HighLow(disnake.ui.View):
     def __init__(self, hint):
         super().__init__(timeout=None)
@@ -23,10 +24,13 @@ class HighLow(disnake.ui.View):
                     except:
                         popcorn = {}
                     try:
-                        popcorn[ctx.author.name + "#" + ctx.author.discriminator]["popcornAmount"] += gains
+                        popcorn[ctx.author.name + "#" +
+                                ctx.author.discriminator]["popcornAmount"] += gains
                     except:
-                        popcorn[ctx.author.name + "#" + ctx.author.discriminator] = {}
-                        popcorn[ctx.author.name + "#" + ctx.author.discriminator]["popcornAmount"] = gains
+                        popcorn[ctx.author.name + "#" +
+                                ctx.author.discriminator] = {}
+                        popcorn[ctx.author.name + "#" +
+                                ctx.author.discriminator]["popcornAmount"] = gains
                     file2.write(json.dumps(popcorn))
         else:
             await ctx.send(f"You lost! The number was {self.num}. Your hint was {self.hint}")
@@ -43,14 +47,17 @@ class HighLow(disnake.ui.View):
                     except:
                         popcorn = {}
                     try:
-                        popcorn[ctx.author.name + "#" + ctx.author.discriminator]["popcornAmount"] += gains
+                        popcorn[ctx.author.name + "#" +
+                                ctx.author.discriminator]["popcornAmount"] += gains
                     except:
-                        popcorn[ctx.author.name + "#" + ctx.author.discriminator] = {}
-                        popcorn[ctx.author.name + "#" + ctx.author.discriminator]["popcornAmount"] = gains
+                        popcorn[ctx.author.name + "#" +
+                                ctx.author.discriminator] = {}
+                        popcorn[ctx.author.name + "#" +
+                                ctx.author.discriminator]["popcornAmount"] = gains
                     file2.write(json.dumps(popcorn))
         else:
             await ctx.send(f"You lost! The number was {self.num}. Your hint was {self.hint}")
-    
+
     @disnake.ui.button(label="Higher", style=disnake.ButtonStyle.red, custom_id="highlow:higher")
     async def higher(self, button: disnake.ui.Button, ctx: disnake.MessageInteraction):
         if self.num > self.hint:
@@ -63,14 +70,16 @@ class HighLow(disnake.ui.View):
                     except:
                         popcorn = {}
                     try:
-                        popcorn[ctx.author.name + "#" + ctx.author.discriminator]["popcornAmount"] += gains
+                        popcorn[ctx.author.name + "#" +
+                                ctx.author.discriminator]["popcornAmount"] += gains
                     except:
-                        popcorn[ctx.author.name + "#" + ctx.author.discriminator] = {}
-                        popcorn[ctx.author.name + "#" + ctx.author.discriminator]["popcornAmount"] = gains
+                        popcorn[ctx.author.name + "#" +
+                                ctx.author.discriminator] = {}
+                        popcorn[ctx.author.name + "#" +
+                                ctx.author.discriminator]["popcornAmount"] = gains
                     file2.write(json.dumps(popcorn))
         else:
             await ctx.send(f"You lost! The number was {self.num}. Your hint was {self.hint}")
-
 
 
 class PopcornCommands(commands.Cog):
@@ -105,65 +114,70 @@ class PopcornCommands(commands.Cog):
 
     @commands.slash_command(description="Checks your popcorn status")
     async def popcornstatus(self, ctx):
-        with open("popcorn.json", "r+") as file:
-            try:
-                popcornStuff = json.loads(file.read())
-            except:
-                popcornStuff = {}
-            member = ctx.author
-            data = 0
-            try:
-                data = popcornStuff[member.name + "#" +
-                                     member.discriminator]["popcornAmount"]
-            except:
-                popcornStuff[member.name + "#" +
-                                     member.discriminator] = {}
-                popcornStuff[member.name + "#" + member.discriminator]["popcornAmount"] = 0
-                data = popcornStuff[member.name + "#" +
-                                     member.discriminator]["popcornAmount"]
-                jsonString = json.dumps(popcornStuff)
-                file.write(jsonString)
-            finally:
-                embed = disnake.Embed(
-                    title="Popcorn Status",
-                    description=f"The amount popcorn {member.name + '#' + member.discriminator} has is {data}"
-                )
-                await ctx.send(embed=embed)
+        with open("popcorn.json", "r") as file:
+            with open("popcorn.json", "w") as file2:
+                try:
+                    popcornStuff = json.loads(file.read())
+                except:
+                    popcornStuff = {}
+                member = ctx.author
+                data = 0
+                try:
+                    data = popcornStuff[member.name + "#" +
+                                        member.discriminator]["popcornAmount"]
+                except:
+                    popcornStuff[member.name + "#" +
+                                 member.discriminator] = {}
+                    popcornStuff[member.name + "#" +
+                                 member.discriminator]["popcornAmount"] = 0
+                    data = popcornStuff[member.name + "#" +
+                                        member.discriminator]["popcornAmount"]
+                    jsonString = json.dumps(popcornStuff)
+                    file2.write(jsonString)
+                finally:
+                    embed = disnake.Embed(
+                        title="Popcorn Status",
+                        description=f"The amount popcorn {member.name + '#' + member.discriminator} has is {data}"
+                    )
+                    await ctx.send(embed=embed)
 
     @commands.slash_command(description="Search for Popcorn")
     async def searchforpopcorn(self, ctx):
-        with open("popcorn.json", "r+") as file:
-            try:
-                popcornStuff = json.loads(file.read())
-            except:
-                popcornStuff = {}
-            place = random.choice(self.places)
-            gains = 0
-            title = f"You searched the {place['name']}"
-            description = f""
-            member = ctx.author
+        with open("popcorn.json", "r") as file:
+            with open("popcorn.json", "w") as file2:
+                try:
+                    popcornStuff = json.loads(file.read())
+                except:
+                    popcornStuff = {}
+                place = random.choice(self.places)
+                gains = 0
+                title = f"You searched the {place['name']}"
+                description = f""
+                member = ctx.author
 
-            if random.randint(1, place["chance"]) == 1:
-                gains = random.randint(place["amountMin"], place["amountMax"])
-            if gains == 0:
-                description = f"LMFAO!!!! You searched the {place['name']} and found not a single piece of popcorn! Imagine not finding a single piece of popcorn. Like, couldn't be me!"
-            else:
-                description = f"Congrats! You found {gains} pieces of popcorn in the {place['name']}"
+                if random.randint(1, place["chance"]) == 1:
+                    gains = random.randint(
+                        place["amountMin"], place["amountMax"])
+                if gains == 0:
+                    description = f"LMFAO!!!! You searched the {place['name']} and found not a single piece of popcorn! Imagine not finding a single piece of popcorn. Like, couldn't be me!"
+                else:
+                    description = f"Congrats! You found {gains} pieces of popcorn in the {place['name']}"
 
-            try:
-                popcornStuff[member.name + "#" +
-                                     member.discriminator]["popcornAmount"] += gains
-            except:
-                popcornStuff[member.name + "#" +
-                                     member.discriminator] = {}
-                popcornStuff[member.name + "#" + member.discriminator]["popcornAmount"] = gains
-            
-            embed = disnake.Embed(
-                title=title,
-                description=description
-            )
+                try:
+                    popcornStuff[member.name + "#" +
+                                 member.discriminator]["popcornAmount"] += gains
+                except:
+                    popcornStuff[member.name + "#" +
+                                 member.discriminator] = {}
+                    popcornStuff[member.name + "#" +
+                                 member.discriminator]["popcornAmount"] = gains
 
-            await ctx.send(embed=embed)
+                embed = disnake.Embed(
+                    title=title,
+                    description=description
+                )
+
+                await ctx.send(embed=embed)
 
     @commands.slash_command(description="Get a chance of winning a lot of popcorn by guessing my number")
     async def highlow(self, ctx):
@@ -173,6 +187,69 @@ class PopcornCommands(commands.Cog):
             description=f"Guess my number and I'll give you some popcorn :popcorn:. Your hint is {hint}"
         )
         await ctx.send(embed=embed, view=HighLow(hint))
+
+    @commands.slash_command(description="Show your inventory. WIP")
+    async def inventory(self, ctx):
+        with open("popcorn.json", "r") as file:
+            try:
+                popcorn = json.loads(file.read())
+            except:
+                popcorn = {}
+            try:
+                inv = popcorn[ctx.author.name + "#" +
+                              ctx.author.discriminator]["inventory"]
+            except:
+                popcorn[ctx.author.name + "#" + ctx.author.discriminator] = {
+                    "popcornAmount": 150,
+                    "inventory": {
+                        "starter-pack": {
+                            "usable": True,
+                            "use": "Just a starter pack for you",
+                            "shovel": {
+                                "usable": True,
+                                "use": "Dig up random stuff with this!"
+                            },
+                            "pickaxe": {
+                                "usable": True,
+                                "use": "I guess when the shovel doesn't work, use this?"
+                            },
+                            "fishing-rod": {
+                                "usable": True,
+                                "use": "Fish up goods"
+                            },
+                            "rifle": {
+                                "usable": True,
+                                "use": "MURDER THE ANIMALS! MURDER THE ANIMALS! MURDER THE ANIMALS!!!!!!"
+                            }
+                        }
+                    }
+                }
+                inv = popcorn[ctx.author.name + "#" +
+                              ctx.author.discriminator]["inventory"]
+                with open("popcorn.json", "w") as file2:
+                    file2.write(json.dumps(popcorn))
+            items = ""
+            for key in inv:
+                if items == "":
+                    tmp = key.split("-")
+                    tmp2 = []
+                    for i in range(len(tmp)):
+                        tmp2.append(tmp[i][0].upper() + tmp[i][1:])
+                    key = ""
+                    for i in range(len(tmp2)):
+                        key += tmp[i] + " "
+                    items += key
+                else:
+                    tmp = key.split("-")
+                    tmp2 = []
+                    for i in range(len(tmp)):
+                        tmp2.append(tmp[i][0].upper() + tmp[i][1:])
+                    key = ""
+                    for i in range(len(tmp2)):
+                        key += tmp[i] + " "
+                    items += "\n" + key
+            await ctx.send(items)
+
 
 def setup(bot):
     bot.add_cog(PopcornCommands(bot))
